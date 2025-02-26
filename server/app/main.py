0fbc6225eba857d9e23bb6289eb91340ec5c58ac
Hersh-SS <hersheysandhu@gmail.com>
2025-02-26 16:45:17 -0500

    Enable CORS in Flask and connect frontend to display tickets from API

--- server/app/main.py
+++ server/app/main.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from app.routes.tickets import tickets_bp
 
 app = Flask(__name__)
+CORS(app)
+
 app.register_blueprint(tickets_bp)
 
 @app.route("/")
